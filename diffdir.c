0ca1d8a6a9fc966d4b55ab1eafcb6db128f48bdf
Jordan Torbiak <torbiak@gmail.com>
2024-09-12 17:09:27 -0600

    Use spaces instead of tabs and wrap lines to 80 chars

--- diffdir.c
+++ diffdir.c
@@ -14,86 +14,87 @@
 static void
 die (int line_number, const char *format, ...)
 {
-	va_list vargs;
-	va_start(vargs, format);
-	fprintf(stderr, "%d: ", line_number);
-	vfprintf(stderr, format, vargs);
-	fprintf(stderr, "\n");
-	va_end(vargs);
-	abort();
+    va_list vargs;
+    va_start(vargs, format);
+    fprintf(stderr, "%d: ", line_number);
+    vfprintf(stderr, format, vargs);
+    fprintf(stderr, "\n");
+    va_end(vargs);
+    abort();
 }
 
 static void*
 xmalloc(size_t size)
 {
-	void *p = malloc(size);
-	if (p == NULL) {
-		die(__LINE__, "out of memory: malloc failed");
-	}
-	return p;
+    void *p = malloc(size);
+    if (p == NULL) {
+        die(__LINE__, "out of memory: malloc failed");
+    }
+    return p;
 }
 
 static char*
 sprintf_alloc(const char *format, ...)
 {
-	char *s;
-	va_list vargs;
-	va_start(vargs, format);
-	int n = vasprintf(&s, format, vargs);
-	va_end(vargs);
-	if (n == -1 || s == NULL) {
-		die(__LINE__, "vasprintf failed: likely out of memory");
-	}
-	return s;
+    char *s;
+    va_list vargs;
+    va_start(vargs, format);
+    int n = vasprintf(&s, format, vargs);
+    va_end(vargs);
+    if (n == -1 || s == NULL) {
+        die(__LINE__, "vasprintf failed: likely out of memory");
+    }
+    return s;
 }
 
 // Join two paths with a slash and return a malloc'd string.
 static char*
 join(const char *root, const char *rel_path)
 {
-	char *joined = NULL;
-	int root_len = strlen(root);
-	int need_slash = root_len > 0 && root[root_len - 1] != '/';
-	joined = sprintf_alloc("%s%s%s", root, need_slash ? "/" : "", rel_path);
-	return joined;
+    char *joined = NULL;
+    int root_len = strlen(root);
+    int need_slash = root_len > 0 && root[root_len - 1] != '/';
+    joined = sprintf_alloc("%s%s%s", root, need_slash ? "/" : "", rel_path);
+    return joined;
 }
 
 static int
 scandir_filter(const struct dirent *dirent)
 {
-	return strcmp(dirent->d_name, ".") != 0 && strcmp(dirent->d_name, "..") != 0;
+    return strcmp(dirent->d_name, ".") != 0
+        && strcmp(dirent->d_name, "..") != 0;
 }
 
 static char*
 scandir_chk(const char *dir, struct dirent ***restrict namelist, int *n)
 {
-	char *err = NULL;
-	*n = scandir(dir, namelist, scandir_filter, alphasort);
-	if (*n == -1) {
-		err = sprintf_alloc("scandir: %s", strerror(errno));
-	}
-	return err;
+    char *err = NULL;
+    *n = scandir(dir, namelist, scandir_filter, alphasort);
+    if (*n == -1) {
+        err = sprintf_alloc("scandir: %s", strerror(errno));
+    }
+    return err;
 }
 
 static char*
 stat_chk(const char *restrict path, struct stat *restrict buf)
 {
-	char *err = NULL;
-	int res = stat(path, buf);
-	if (res == -1) {
-		err = sprintf_alloc("stat %s: %s", path, strerror(errno));
-	}
-	return err;
+    char *err = NULL;
+    int res = stat(path, buf);
+    if (res == -1) {
+        err = sprintf_alloc("stat %s: %s", path, strerror(errno));
+    }
+    return err;
 }
 
 static void
 free_namelist(struct dirent **namelist, int len)
 {
-	int i = len;
-	while (i--) {
-		free(namelist[i]);
-	}
-	free(namelist);
+    int i = len;
+    while (i--) {
+        free(namelist[i]);
+    }
+    free(namelist);
 }
 
 // Return a malloc'd error string on error. On sucess, NULL is returned and
@@ -101,201 +102,204 @@ free_namelist(struct dirent **namelist, int len)
 // otherwise.
 static char*
 cmp_file_contents(
-		const char *filename_a,
-		const char *filename_b,
-		int *is_same)
+        const char *filename_a,
+        const char *filename_b,
+        int *is_same)
 {
-	*is_same = 0;
+    *is_same = 0;
     char *err = NULL;
-	unsigned char *buf_a = xmalloc(BUFSIZ);
-	unsigned char *buf_b = xmalloc(BUFSIZ);
+    unsigned char *buf_a = xmalloc(BUFSIZ);
+    unsigned char *buf_b = xmalloc(BUFSIZ);
 
-	FILE *file_a = fopen(filename_a, "r");
-	if (file_a == NULL) {
-		err = sprintf_alloc("open %s: %s", filename_a, strerror(errno));
+    FILE *file_a = fopen(filename_a, "r");
+    if (file_a == NULL) {
+        err = sprintf_alloc("open %s: %s", filename_a, strerror(errno));
         goto cleanup;
-	}
-	FILE *file_b = fopen(filename_b, "r");
-	if (file_b == NULL) {
-		err = sprintf_alloc("open %s: %s", filename_b, strerror(errno));
+    }
+    FILE *file_b = fopen(filename_b, "r");
+    if (file_b == NULL) {
+        err = sprintf_alloc("open %s: %s", filename_b, strerror(errno));
         goto cleanup;
-	}
+    }
 
-	int eof_a = 0, eof_b = 0;
-	while (!eof_a && !eof_b) {
-		int n_a = fread(buf_a, 1, BUFSIZ, file_a);
-		if (n_a < BUFSIZ) {
-			if (ferror(file_a)) {
-				err = sprintf_alloc("error reading file: %s", filename_a);
+    int eof_a = 0, eof_b = 0;
+    while (!eof_a && !eof_b) {
+        int n_a = fread(buf_a, 1, BUFSIZ, file_a);
+        if (n_a < BUFSIZ) {
+            if (ferror(file_a)) {
+                err = sprintf_alloc("error reading file: %s", filename_a);
                 goto cleanup;
-			} else {
-				eof_a = 1;
-			}
-		}
-		int n_b = fread(buf_b, 1, BUFSIZ, file_b);
-		if (n_b < BUFSIZ) {
-			if (ferror(file_b)) {
-				err = sprintf_alloc("error reading file: %s", filename_b);
+            } else {
+                eof_a = 1;
+            }
+        }
+        int n_b = fread(buf_b, 1, BUFSIZ, file_b);
+        if (n_b < BUFSIZ) {
+            if (ferror(file_b)) {
+                err = sprintf_alloc("error reading file: %s", filename_b);
                 goto cleanup;
-			} else {
-				eof_b = 1;
-			}
-		}
+            } else {
+                eof_b = 1;
+            }
+        }
 
-		if (n_a != n_b) {
+        if (n_a != n_b) {
             goto cleanup;
-		}
+        }
 
-		if (memcmp(buf_a, buf_b, n_a)) {
+        if (memcmp(buf_a, buf_b, n_a)) {
             goto cleanup;
-		}
-	}
-	*is_same = 1;
+        }
+    }
+    *is_same = 1;
 
 cleanup:
-	if (buf_a) free(buf_a);
-	if (buf_b) free(buf_b);
-	return err;
+    if (buf_a) free(buf_a);
+    if (buf_b) free(buf_b);
+    return err;
 }
 
 // Return NULL on success, or a malloc'd error message on error.
 static char*
 _diffdir(
-	const char *root_a,
-	const char *rel_dir_a,
-	const char *root_b,
-	const char *rel_dir_b,
-	FILE *common,
-	FILE *a_only,
-	FILE *b_only)
+    const char *root_a,
+    const char *rel_dir_a,
+    const char *root_b,
+    const char *rel_dir_b,
+    FILE *common,
+    FILE *a_only,
+    FILE *b_only)
 {
-	// Path to the current dir, including the root.
-	char *dir_a = NULL, *dir_b = NULL;
-	// Path to the current file, including the root.
-	char *path_a = NULL, *path_b = NULL;
-	// Path to the current file, relative to the root.
-	char *rel_path_a = NULL, *rel_path_b = NULL;
-	char *err = NULL;
+    // Path to the current dir, including the root.
+    char *dir_a = NULL, *dir_b = NULL;
+    // Path to the current file, including the root.
+    char *path_a = NULL, *path_b = NULL;
+    // Path to the current file, relative to the root.
+    char *rel_path_a = NULL, *rel_path_b = NULL;
+    char *err = NULL;
 
-	dir_a = join(root_a, rel_dir_a);
-	dir_b = join(root_b, rel_dir_b);
+    dir_a = join(root_a, rel_dir_a);
+    dir_b = join(root_b, rel_dir_b);
 
-	struct dirent **names_a = NULL, **names_b = NULL;
-	int n_a = 0, n_b = 0;
-	err = scandir_chk(dir_a, &names_a, &n_a);
-	if (err) goto cleanup;
-	err = scandir_chk(dir_b, &names_b, &n_b);
-	if (err) goto cleanup;
+    struct dirent **names_a = NULL, **names_b = NULL;
+    int n_a = 0, n_b = 0;
+    err = scandir_chk(dir_a, &names_a, &n_a);
+    if (err) goto cleanup;
+    err = scandir_chk(dir_b, &names_b, &n_b);
+    if (err) goto cleanup;
 
-	int i_a = 0, i_b = 0;
-	while (i_a < n_a || i_b < n_b) {
-		// If we run out of names on one side, use up the other side.
-		if (i_b >= n_b) {
-			if (rel_path_a) free(rel_path_a);
-			rel_path_a = join(rel_dir_a, names_a[i_a]->d_name);
-			fprintf(a_only, "%s\n", rel_path_a);
-			++i_a;
-			continue;
-		} else if (i_b >= n_b) {
-			if (rel_path_b) free(rel_path_b);
-			rel_path_b = join(rel_dir_b, names_b[i_b]->d_name);
-			fprintf(b_only, "%s\n", rel_path_b);
-			++i_b;
-			continue;
-		} else {
-			if (rel_path_a) free(rel_path_a);
-			rel_path_a = join(rel_dir_a, names_a[i_a]->d_name);
-			if (rel_path_b) free(rel_path_b);
-			rel_path_b = join(rel_dir_b, names_b[i_b]->d_name);
-		}
+    int i_a = 0, i_b = 0;
+    while (i_a < n_a || i_b < n_b) {
+        // If we run out of names on one side, use up the other side.
+        if (i_b >= n_b) {
+            if (rel_path_a) free(rel_path_a);
+            rel_path_a = join(rel_dir_a, names_a[i_a]->d_name);
+            fprintf(a_only, "%s\n", rel_path_a);
+            ++i_a;
+            continue;
+        } else if (i_b >= n_b) {
+            if (rel_path_b) free(rel_path_b);
+            rel_path_b = join(rel_dir_b, names_b[i_b]->d_name);
+            fprintf(b_only, "%s\n", rel_path_b);
+            ++i_b;
+            continue;
+        } else {
+            if (rel_path_a) free(rel_path_a);
+            rel_path_a = join(rel_dir_a, names_a[i_a]->d_name);
+            if (rel_path_b) free(rel_path_b);
+            rel_path_b = join(rel_dir_b, names_b[i_b]->d_name);
+        }
 
-		// If a name is only on one side, write it to the appropriate file and
-		// continue.
-		int cmp = strcmp(names_a[i_a]->d_name, names_b[i_b]->d_name);
-		if (cmp < 0) {
-			fprintf(a_only, "%s\n", rel_path_a);
-			++i_a;
-			continue;
-		} else if (cmp > 0) {
-			fprintf(b_only, "%s\n", rel_path_b);
-			++i_b;
-			continue;
-		}
+        // If a name is only on one side, write it to the appropriate file and
+        // continue.
+        int cmp = strcmp(names_a[i_a]->d_name, names_b[i_b]->d_name);
+        if (cmp < 0) {
+            fprintf(a_only, "%s\n", rel_path_a);
+            ++i_a;
+            continue;
+        } else if (cmp > 0) {
+            fprintf(b_only, "%s\n", rel_path_b);
+            ++i_b;
+            continue;
+        }
 
-		// The same name is on both sides: choose whether to recurse into dirs
-		// and compare bytes of files if needed.
+        // The same name is on both sides: choose whether to recurse into dirs
+        // and compare bytes of files if needed.
 
-		if (path_a) free(path_a);
-		path_a = join(dir_a, names_a[i_a]->d_name);
-		if (path_b) free(path_b);
-		path_b = join(dir_b, names_b[i_b]->d_name);
-		if (strchr(path_a, '\n') || strchr(path_b, '\n')) {
-			// The output files are newline-delimited, so filenames that
-			// contain newlines would corrupt the output.
-			err = sprintf_alloc("error: filename contains newline");
-			goto cleanup;
-		}
-		
-		struct stat stat_a, stat_b;
-		err = stat_chk(path_a, &stat_a);
-		if (err) goto cleanup;
-		err = stat_chk(path_b, &stat_b);
-		if (err) goto cleanup;
+        if (path_a) free(path_a);
+        path_a = join(dir_a, names_a[i_a]->d_name);
+        if (path_b) free(path_b);
+        path_b = join(dir_b, names_b[i_b]->d_name);
+        if (strchr(path_a, '\n') || strchr(path_b, '\n')) {
+            // The output files are newline-delimited, so filenames that
+            // contain newlines would corrupt the output.
+            err = sprintf_alloc("error: filename contains newline");
+            goto cleanup;
+        }
 
-		int is_same = 0;
-		int is_dir_a = S_ISDIR(stat_a.st_mode);
-		int is_dir_b = S_ISDIR(stat_b.st_mode);
-		if (is_dir_a && is_dir_b) {
-			is_same = 1;
-		} else if (stat_a.st_size == stat_b.st_size) {
-			err = cmp_file_contents(path_a, path_b, &is_same);
-			if (err) goto cleanup;
-		}
-		if (is_same) {
-			// Same name and contents.
-			fprintf(common, "%s\n", rel_path_a);
-		} else {
-			// Same name on both sides, but they don't have the same contents,
-			// or one is a dir and the other isn't.
-			fprintf(a_only, "%s\n", rel_path_a);
-			fprintf(b_only, "%s\n", rel_path_b);
-		}
-		if (is_same && is_dir_a && is_dir_b) {
-			// Both sides are dirs: recurse.
-			err = _diffdir(dir_a, rel_path_a, dir_b, rel_path_b, common, a_only, b_only);
-			if (err) goto cleanup;
-		}
-		++i_a;
-		++i_b;
-	}
+        struct stat stat_a, stat_b;
+        err = stat_chk(path_a, &stat_a);
+        if (err) goto cleanup;
+        err = stat_chk(path_b, &stat_b);
+        if (err) goto cleanup;
+
+        int is_same = 0;
+        int is_dir_a = S_ISDIR(stat_a.st_mode);
+        int is_dir_b = S_ISDIR(stat_b.st_mode);
+        if (is_dir_a && is_dir_b) {
+            is_same = 1;
+        } else if (stat_a.st_size == stat_b.st_size) {
+            err = cmp_file_contents(path_a, path_b, &is_same);
+            if (err) goto cleanup;
+        }
+        if (is_same) {
+            // Same name and contents.
+            fprintf(common, "%s\n", rel_path_a);
+        } else {
+            // Same name on both sides, but they don't have the same contents,
+            // or one is a dir and the other isn't.
+            fprintf(a_only, "%s\n", rel_path_a);
+            fprintf(b_only, "%s\n", rel_path_b);
+        }
+        if (is_same && is_dir_a && is_dir_b) {
+            // Both sides are dirs: recurse.
+            err = _diffdir(
+                    dir_a, rel_path_a,
+                    dir_b, rel_path_b,
+                    common, a_only, b_only);
+            if (err) goto cleanup;
+        }
+        ++i_a;
+        ++i_b;
+    }
 
 cleanup:
-	if (dir_a) free(dir_a);
-	if (dir_b) free(dir_b);
-	if (names_a) free_namelist(names_a, n_a);
-	if (names_b) free_namelist(names_b, n_b);
-	if (path_a) free(path_a);
-	if (path_b) free(path_b);
-	if (rel_path_a) free(rel_path_a);
-	if (rel_path_b) free(rel_path_b);
-	return err;
+    if (dir_a) free(dir_a);
+    if (dir_b) free(dir_b);
+    if (names_a) free_namelist(names_a, n_a);
+    if (names_b) free_namelist(names_b, n_b);
+    if (path_a) free(path_a);
+    if (path_b) free(path_b);
+    if (rel_path_a) free(rel_path_a);
+    if (rel_path_b) free(rel_path_b);
+    return err;
 }
 
 // Return NULL on success, or a malloc'd error message on error.
 char*
 diffdir(
-	const char *dir_a,
-	const char *dir_b,
-	FILE *common,
-	FILE *a_only,
-	FILE *b_only)
+    const char *dir_a,
+    const char *dir_b,
+    FILE *common,
+    FILE *a_only,
+    FILE *b_only)
 {
-	char *err = _diffdir(dir_a, "", dir_b, "", common, a_only, b_only);
-	if (err) {
-		char *wrap_err = NULL;
-		wrap_err = sprintf_alloc("diffdir: %s", err);
-		free(err);
-		return wrap_err;
-	}
-	return err;
+    char *err = _diffdir(dir_a, "", dir_b, "", common, a_only, b_only);
+    if (err) {
+        char *wrap_err = NULL;
+        wrap_err = sprintf_alloc("diffdir: %s", err);
+        free(err);
+        return wrap_err;
+    }
+    return err;
 }
--- main.c
+++ main.c
@@ -15,95 +15,96 @@ static char usage[] = "usage: diffdir [-h|--help] <dir_a> <dir_b>\n";
 static FILE*
 open_writable_file(const char *name)
 {
-	FILE *f = fopen(name, "w");
-	if (f == NULL) {
-		fprintf(stderr, "open %s: %s\n", name, strerror(errno));
-		perror(NULL);
-	}
-	return f;
+    FILE *f = fopen(name, "w");
+    if (f == NULL) {
+        fprintf(stderr, "open %s: %s\n", name, strerror(errno));
+        perror(NULL);
+    }
+    return f;
 }
 
 // Close a file and log an error, if any, to stderr.
 static void
 close_file(FILE *f, const char *name)
 {
-	int err = fclose(f);
-	if (err != 0) {
-		fprintf(stderr, "close %s: %s\n", name, strerror(errno));
-		perror(NULL);
-	}
+    int err = fclose(f);
+    if (err != 0) {
+        fprintf(stderr, "close %s: %s\n", name, strerror(errno));
+        perror(NULL);
+    }
 }
 
 // Check that the given filename can be accessed and exit if not.
 static void
 dir_accessible_or_die(char *filename)
 {
-	if (access(filename, R_OK) != 0) {
-		fprintf(stderr, "directory %s not accessible: %s\n", filename, strerror(errno));
-		exit(EXIT_FAILURE);
-	}
+    if (access(filename, R_OK) != 0) {
+        fprintf(stderr, "directory %s not accessible: %s\n",
+                filename, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 }
 
 int
 main(int argc, char *argv[])
 {
-	int c;
-	int help_flag = 0;
-	struct option long_options[] = {
-		{"help", no_argument, &help_flag, 1},
-		{0, 0, 0, 0}
-	};
-	while ((c = getopt_long(argc, argv, "h", long_options, NULL)) != -1) {
-		switch (c) {
-			case 'h':
-				help_flag = 1;
-				break;
-			case '?':
-				// getopt prints its own error message
-				exit(EXIT_FAILURE);
-			default:
-				break;
-		}
-	}
-	if (help_flag) {
-		fputs(usage, stdout);
-		exit(EXIT_SUCCESS);
-	}
-	if (argc - optind < 2) {
-		fputs(usage, stderr);
-		fputs("\nwrong number of arguments\n", stderr);
-		exit(EXIT_FAILURE);
-	}
-	char *dir_a = argv[optind++];
-	char *dir_b = argv[optind++];
+    int c;
+    int help_flag = 0;
+    struct option long_options[] = {
+        {"help", no_argument, &help_flag, 1},
+        {0, 0, 0, 0}
+    };
+    while ((c = getopt_long(argc, argv, "h", long_options, NULL)) != -1) {
+        switch (c) {
+            case 'h':
+                help_flag = 1;
+                break;
+            case '?':
+                // getopt prints its own error message
+                exit(EXIT_FAILURE);
+            default:
+                break;
+        }
+    }
+    if (help_flag) {
+        fputs(usage, stdout);
+        exit(EXIT_SUCCESS);
+    }
+    if (argc - optind < 2) {
+        fputs(usage, stderr);
+        fputs("\nwrong number of arguments\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+    char *dir_a = argv[optind++];
+    char *dir_b = argv[optind++];
 
-	// Check that the given directories exist before making output files.
-	dir_accessible_or_die(dir_a);
-	dir_accessible_or_die(dir_b);
+    // Check that the given directories exist before making output files.
+    dir_accessible_or_die(dir_a);
+    dir_accessible_or_die(dir_b);
 
-	int rc = EXIT_SUCCESS;
-	char *err = NULL;
-	FILE *common = NULL, *a_only = NULL, *b_only = NULL;
+    int rc = EXIT_SUCCESS;
+    char *err = NULL;
+    FILE *common = NULL, *a_only = NULL, *b_only = NULL;
 
-	common = open_writable_file("common");
-	if (common == NULL) { rc = EXIT_FAILURE; goto cleanup; }
-	a_only = open_writable_file("a_only");
-	if (common == NULL) { rc = EXIT_FAILURE; goto cleanup; }
-	b_only = open_writable_file("b_only");
-	if (common == NULL) { rc = EXIT_FAILURE; goto cleanup; }
+    common = open_writable_file("common");
+    if (common == NULL) { rc = EXIT_FAILURE; goto cleanup; }
+    a_only = open_writable_file("a_only");
+    if (common == NULL) { rc = EXIT_FAILURE; goto cleanup; }
+    b_only = open_writable_file("b_only");
+    if (common == NULL) { rc = EXIT_FAILURE; goto cleanup; }
 
-	err = diffdir(dir_a, dir_b, common, a_only, b_only);
-	if (err) {
-		fprintf(stderr, "%s\n", err);
-		rc = EXIT_FAILURE;
-		goto cleanup;
-	}
+    err = diffdir(dir_a, dir_b, common, a_only, b_only);
+    if (err) {
+        fprintf(stderr, "%s\n", err);
+        rc = EXIT_FAILURE;
+        goto cleanup;
+    }
 
 cleanup:
-	if (err) free(err);
-	if (common) close_file(common, "common");
-	if (a_only) close_file(a_only, "a_only");
-	if (b_only) close_file(b_only, "b_only");
+    if (err) free(err);
+    if (common) close_file(common, "common");
+    if (a_only) close_file(a_only, "a_only");
+    if (b_only) close_file(b_only, "b_only");
 
-	return rc;
+    return rc;
 }
