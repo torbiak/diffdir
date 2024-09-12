c9453db54c08af4f3e20935c520ee3a6378fcb20
Jordan Torbiak <torbiak@gmail.com>
2024-09-12 16:46:17 -0600

    Add -h|--help flags and improve argument handling

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,20 +7,7 @@
 
 #include "diffdir.h"
 
-// Return zero on success, and nonzero on error.
-static int
-parse_args(int argc, char *argv[], char **dir_a, char **dir_b)
-{
-	// TODO
-	if (argc != 3) {
-		fprintf(stderr, "wrong number of args");
-		exit(1);
-	}
-	*dir_a = argv[1];
-	*dir_b = argv[2];
-	return 0;  // TODO
-}
-
+static char usage[] = "usage: diffdir [-h|--help] <dir_a> <dir_b>\n";
 
 // Return a FILE pointer on success, and NULL on error.
 static FILE*
@@ -44,30 +32,68 @@ close_file(FILE *f, const char *name)
 	}
 }
 
+// Check that the given filename can be accessed and exit if not.
+static void
+dir_accessible_or_die(char *filename)
+{
+	if (access(filename, R_OK) != 0) {
+		fprintf(stderr, "directory %s not accessible: %s\n", filename, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+}
+
 int
 main(int argc, char *argv[])
 {
-	char *dir_a = NULL, *dir_b = NULL;
+	int c;
+	int help_flag = 0;
+	struct option long_options[] = {
+		{"help", no_argument, &help_flag, 1},
+		{0, 0, 0, 0}
+	};
+	while ((c = getopt_long(argc, argv, "h", long_options, NULL)) != -1) {
+		switch (c) {
+			case 'h':
+				help_flag = 1;
+				break;
+			case '?':
+				// getopt prints its own error message
+				exit(EXIT_FAILURE);
+			default:
+				break;
+		}
+	}
+	if (help_flag) {
+		fputs(usage, stdout);
+		exit(EXIT_SUCCESS);
+	}
+	if (argc - optind < 2) {
+		fputs(usage, stderr);
+		fputs("\nwrong number of arguments\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+	char *dir_a = argv[optind++];
+	char *dir_b = argv[optind++];
+
+	// Check that the given directories exist before making output files.
+	dir_accessible_or_die(dir_a);
+	dir_accessible_or_die(dir_b);
+
 	int rc = EXIT_SUCCESS;
 	char *err = NULL;
 	FILE *common = NULL, *a_only = NULL, *b_only = NULL;
 
-	int err_int = parse_args(argc, argv, &dir_a, &dir_b);
-	if (err_int) {
-		rc = EXIT_FAILURE;
-		goto cleanup;
-	}
-
 	common = open_writable_file("common");
-	if (common == NULL) { goto cleanup; }
+	if (common == NULL) { rc = EXIT_FAILURE; goto cleanup; }
 	a_only = open_writable_file("a_only");
-	if (a_only == NULL) { goto cleanup; }
+	if (common == NULL) { rc = EXIT_FAILURE; goto cleanup; }
 	b_only = open_writable_file("b_only");
-	if (b_only == NULL) { goto cleanup; }
+	if (common == NULL) { rc = EXIT_FAILURE; goto cleanup; }
 
 	err = diffdir(dir_a, dir_b, common, a_only, b_only);
 	if (err) {
 		fprintf(stderr, "%s\n", err);
+		rc = EXIT_FAILURE;
 		goto cleanup;
 	}
 
